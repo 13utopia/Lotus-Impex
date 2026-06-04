a72a0dd8d5ecaa78f8d6db3762f3358c49ce95a8
smitradadiya001 <smitradadiya2005@gmail.com>
2026-06-04 16:31:46 +0530

    dummy commit

--- lotus-impex/src/pages/Home.jsx
+++ lotus-impex/src/pages/Home.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import Hero from '../components/Hero/Hero';
-import ProductCategories from '../components/ProductCategories/ProductCategories';
-import Statistics from '../components/Statistics/Statistics';
-import Industries from '../components/Industries/Industries';
-import ProjectSection from '../components/ProjectSection/ProjectSection';
-import FeaturedProducts from '../components/FeaturedProducts/FeaturedProducts';
-import Testimonials from '../components/Testimonials/Testimonials';
-import BlogSection from '../components/BlogSection/BlogSection';
-import Workflow from '../components/Workflow/Workflow';
-import Banner from '../components/Banner/Banner';
-import Enterprise from '../components/Enterprise/Enterprise';
+import React from "react";
+import Hero from "../components/Hero/Hero";
+import ProductCategories from "../components/ProductCategories/ProductCategories";
+import Statistics from "../components/Statistics/Statistics";
+import Industries from "../components/Industries/Industries";
+import ProjectSection from "../components/ProjectSection/ProjectSection";
+import FeaturedProducts from "../components/FeaturedProducts/FeaturedProducts";
+import Testimonials from "../components/Testimonials/Testimonials";
+import BlogSection from "../components/BlogSection/BlogSection";
+import Workflow from "../components/Workflow/Workflow";
+import Banner from "../components/Banner/Banner";
+import Enterprise from "../components/Enterprise/Enterprise";
 
 const Home = () => {
   return (
